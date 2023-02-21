6cc1ac02f0ba3e5e843eeb3f1f45a6a500ee27a5
sourabsarkar2907 <101975530+sourabsarkar2907@users.noreply.github.com>
2023-02-21 09:37:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am fine";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("what's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "My Name is sourab";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("who are you") ||
+      text.includes("what is your work")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "i am your personal listener,i can hear anything";
+      texts.appendChild(p);
+    }
+    if (text.includes("open my YouTube")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening youtube channel";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://www.youtube.com/channel/UCL9TTHDugnlCT2spX2AEK7Q");
+    }
+      
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+recognition.start();
